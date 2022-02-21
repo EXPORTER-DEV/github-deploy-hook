46b886490e18cccf4e86fe5fa8ace0cecdd3693d
Vadim <mrdrakan@Mail.ru>
2022-02-21 13:38:43 +0300

    Initial upload.

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # github-deploy-hook
-NodeJS Express deploy hook for GitHub
+NodeJS Express deploy hook application for GitHub, that checks new `push` events and handle it with ./config.json file:
+1. Search repositoriy with its full_name (example: `USER-NAME/project`).
+2. Checking target event branch equal to config repository branch.
+3. Prepare command with navigation to directory, making `git pull` and then execute your optional command (`command` key in ./config.json -> `repositories` array).
+
+### Configuration
+
+For starting application, you should make ./config.json file, like ./config.example.json in application directory.
+
+```json
+{
+    "port": 3000,
+    "secret": "secret_key",
+    "repositories": [
+        {
+            "full_name": "USER-NAME/project",
+            "path": "/home/user/project/",
+            "branch": "main",
+            "command": "sh ./start.sh"
+        }
+    ]
+}
+```
+
+`port` - express web server running port (optional, if not set - 3000).
+`secret` - secret key (optional, if not set just skip authentication).
+`repositories` - array of repositories with objects as example: `{"full_name": "USER-NAME/project", "path": "/home/user/project/", "branch": "main", "command": "sh ./start.sh"}`.
+
+All properties for `repositories` array are required (as in example - all keys should be presented).
+
+### Running
+
+Install all required dependecies:
+
+```bash
+npm ci
+```
+
+Preffered to start the github-deploy-hook with PM2 (https://www.npmjs.com/package/pm2).
+
+After pm2 successfully installation and configuration, just run:
+
+```bash
+$ pm2 start main.js
+```
+
+Then run command to save that application running after server reboot or etc:
+
+```bash
+$ pm2 save
+```
+
+To see logs of application, write:
+```
+$ pm2 monit
+```
---
+++ main.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const crypto = require('crypto');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+let config = undefined;
+try {
+    config = require('./config.json');
+}catch(e){
+    throw new Error(`Failed to read ./config.json file: ${e.stack}`);
+}
+
+const repositories = config.repositories.map((item) => {
+    if(item?.path !== undefined && item?.branch !== undefined && item?.command !== undefined && item?.full_name !== undefined){
+        return {
+            path: item.path,
+            branch: item.branch,
+            command: item.command,
+            full_name: item.full_name
+        }
+    }
+    return undefined;
+}).filter((item) => item !== undefined);
+
+const secret = config.secret ?? false;
+
+if(repositories.length === 0){
+    throw new Error(`Can't start with 0 correct repositories, check repositories format in ./config.json`);
+}
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('trust proxy', true);
+
+app.post('/', async (req, res) => {
+    const signature = req.header("X-Hub-Signature-256") ?? false;
+    const event = req.header("X-GitHub-Event") ?? false;
+    if(secret !== false){
+        const hash = `sha256=${crypto.createHmac('sha256', secret).update(JSON.stringify(req.body)).digest('hex')}`;
+        if(hash !== signature){
+            console.log(`Forbidden request for: ${req.ip}`);
+            console.log(JSON.stringify(req.body));
+            return res.status(403).send();
+        }
+    }
+
+    console.log(`Got incoming request (${event ?? 'undefined'} event) from ${req.ip} with payload:`);
+    console.log(req.body);
+
+    switch(event){
+        case 'push':
+            for(const repository of repositories){
+                if(repository.branch === req.body?.ref && repository.full_name === req.body?.repository?.full_name){
+                    console.log(`Found repository in config: ${repository.full_name}, for branch: ${repository.branch} - commit ${req.body.head_commit.id} (${req.body.head_commit.message}) from ${req.body.head_commit.timestamp} by ${req.body.head_commit.committer.username}.`);
+                    const command = `cd ${repository.path} && git pull && ${repository.command}`;
+                    console.log(`Starting executing command: ${command}`);
+                    let stdout = false;
+                    let stderr = false;
+                    try {
+                        const execute = await exec(command);
+                        stdout = execute.stdout;
+                        stderr = execute.stderr.length > 0 ?? false;
+                    }catch(e){
+                        console.log(`Failed while handling event: ${e.stack}`);
+                        stderr = true;
+                    }
+                    if(stderr !== false){
+                        console.log(`Failed to handle event.`);
+                        return res.status(500).send(`Failed handle event.`);
+                    }
+                    console.log(`Command result: ${stdout}`);
+                    return res.status(200).send(`Updated ${repository.branch}, executed command: ${repository.command}`);
+                }
+            }
+            console.log(`Doing nothing, could find target repository command in config: ${req.body?.repository.full_name}, for branch: ${req.body?.ref}.`);
+        break;
+        default:
+            console.log(`No handler for ${event} specified.`);
+    }
+    
+    res.status(200).send();
+});
+
+app.listen(config.port ?? 3000, () => {
+    console.log(`Running github-deploy-hook on ${config.port ?? 3000} port.`);
+});
